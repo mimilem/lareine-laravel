f3a8d2b9670d183b6cc2393990ff3b1be39074eb
= <=>
2022-01-29 01:04:09 +0200

    [ADD] Add about page content

---
+++ resources/views/about.blade.php
@@ -0,0 +1,229 @@
+@extends('layouts.master')
+@section('content')
+    <div class="stunning-header stunning-header-bg-lightblue">
+        <div class="stunning-header-content">
+            <h1 class="stunning-header-title">A propos de nous</h1>
+            <ul class="breadcrumbs">
+                <li class="breadcrumbs-item">
+                    <a href="index.html">Accueil</a>
+                    <i class="seoicon-right-arrow"></i>
+                </li>
+                <li class="breadcrumbs-item active">
+                    <span>A propos</span>
+                    <i class="seoicon-right-arrow"></i>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row pt120">
+            <div class="col-lg-12">
+                <div class="heading mb30">
+                    <h4 class="h1 heading-title">Qui sommes-nous ?</h4>
+                    <div class="heading-line">
+                        <span class="short-line"></span>
+                        <span class="long-line"></span>
+                    </div>
+
+                    <h5 class="heading-subtitle">
+                        Fondée en 2012 à Johannesburg, La Souveraine est une agence internationale de Marketing
+                        Stratégique spécialisée dans les solutions de fidélisation client, de développement de
+                        marque et de communication intégrée dans différents secteurs. Nous proposons également des
+                        séminaires de formation internes et publics en marketing et en vente. Nous réalisons pour
+                        nos clients une large gamme d'enquêtes et travaillons en partenariat avec diverses sociétés
+                        spécialisées dans la publicité, le marketing numérique, la conception Web, la gestion de contenu,
+                        conception graphique, etc. pour offrir à nos clients des solutions complètes adaptées à leurs objectifs
+                        spécifiques.
+                    </h5>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row bg-blue-color">
+            <div class="our-vision scrollme">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+                            <div class="heading align-center">
+                                <h4 class="h1 heading-title">Notre mission</h4>
+                                <div class="heading-line">
+                                    <span class="short-line bg-yellow-color"></span>
+                                    <span class="long-line bg-yellow-color"></span>
+                                </div>
+                                <p class="heading-text c-white">Mirum est notare quam littera gothica, quam nunc putamus parum
+                                    claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta
+                                    decima.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <img loading="lazy" src="assets/img/elements.png" class="elements" alt="elements">
+                <img loading="lazy" src="assets/img/info-box12.png" class="eye" alt="eye">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row pt120 mb30">
+
+            <div class="col-lg-12">
+                <div class="heading align-center">
+                    <h4 class="h1 heading-title">Notre Equipe</h4>
+                    <div class="heading-line">
+                        <span class="short-line"></span>
+                        <span class="long-line"></span>
+                    </div>
+                    <p class="heading-text">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium.</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row pb120">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="teammembers-item">
+                    <img loading="lazy" src="assets/img/teammember1.png" alt="team member">
+                    <h5 class="teammembers-item-name">Peter Spenser</h5>
+                    <p class="teammembers-item-prof">Copywriter</p>
+
+                    <div class="socials">
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/circle-facebook.svg" alt="facebook">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/twitter.svg" alt="twitter">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/google.svg" alt="google">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/youtube.svg" alt="youtube">
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="teammembers-item">
+                    <img loading="lazy" src="assets/img/teammember2.png" alt="team member">
+                    <h5 class="teammembers-item-name">Angelina Johnson</h5>
+                    <p class="teammembers-item-prof">SEO Specialist</p>
+
+                    <div class="socials">
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/circle-facebook.svg" alt="facebook">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/twitter.svg" alt="twitter">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/google.svg" alt="google">
+                        </a>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="teammembers-item">
+                    <img loading="lazy" src="assets/img/teammember3.png" alt="team member">
+                    <h5 class="teammembers-item-name">Philip Demarco</h5>
+                    <p class="teammembers-item-prof">Senior Developer</p>
+
+                    <div class="socials">
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/circle-facebook.svg" alt="facebook">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/twitter.svg" alt="twitter">
+                        </a>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="teammembers-item">
+                    <img loading="lazy" src="assets/img/teammember4.png" alt="team member">
+                    <h5 class="teammembers-item-name">James Anderson</h5>
+                    <p class="teammembers-item-prof">Business Analyst</p>
+
+                    <div class="socials">
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/circle-facebook.svg" alt="facebook">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/twitter.svg" alt="twitter">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/google.svg" alt="google">
+                        </a>
+
+                        <a href="#" class="social__item">
+                            <img loading="lazy" src="assets/svg/youtube.svg" alt="youtube">
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section bg-greendark-color">
+
+        <div class="container">
+
+            <div class="slider-profit-wrap">
+
+                <!-- Slider main container -->
+                <div class="swiper-container auto-height " data-direction="vertical" data-loop="false" data-mouse-scroll="true">
+                    <div class="swiper-wrapper">
+                        <div class="slider-profit swiper-slide">
+                            <div class="row medium-padding120">
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <div class="heading">
+                                        <h4 class="h1 heading-title c-white mb30">Des questions ?</h4>
+                                        <p class="c-white">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                                            nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat wisi enim ad veniam.
+                                        </p>
+                                    </div>
+
+                                    <a href="contact.html" class="btn btn-medium btn--dark btn-hover-shadow">
+                                        <span class="text">Entrons en contact</span>
+                                    </a>
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <div class="slider-profit-thumb">
+                                        <img loading="lazy" src="assets/img/profit.png" alt="profit">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
