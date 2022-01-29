f596b2f61eca7574f24178fcfb188f29cc79947b
= <=>
2022-01-29 09:45:09 +0200

    [ADD] Add courses page content

---
+++ resources/views/courses/courses.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+@section('content')
+    <div class="container-fluid">
+        <div class="row medium-padding120 bg-border-color">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                        <div class="heading mb30">
+                            <h4 class="h1 heading-title">Nos missions de Formation</h4>
+                            <div class="heading-line">
+                                <span class="short-line"></span>
+                                <span class="long-line"></span>
+                            </div>
+
+                            <h5 class="heading-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                Impedit, ipsa? Incidunt dolorum quibusdam temporibus inventore quo deserunt sit, 
+                                excepturi maiores quae qui impedit, aspernatur quis assumenda, deleniti molestiae ea ullam?
+                            </h5>
+
+                        </div>
+
+                        <ul class="list list--secondary mb60">
+                            <li>
+                                <i class="seoicon-check"></i>
+                                <span href="#">Renforcement des capacités
+                                </span>
+                            </li>
+
+                            <li>
+                                <i class="seoicon-check"></i>
+                                <span href="#">Management des Equipes
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-12 col-xs-12">
+                        <img loading="lazy" src="/assets/img/social-photo.png" alt="social">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
