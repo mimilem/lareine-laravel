c59d58fd0f6c0d2509c66e168d33fda31d8095df
= <=>
2022-01-31 16:28:22 +0200

    [NEW] Implement confirmation mail sending

---
+++ resources/views/emails/subscribe-confirmation.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email Test</title>
+</head>
+<body>
+    <h1>Email Test - La Souveraine</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum architecto ipsam recusandae laborum unde nisi quod molestiae eveniet, tempora labore odit, saepe dolorem accusantium earum at! Quam perferendis cumque minus.</p>
+</body>
+</html>
