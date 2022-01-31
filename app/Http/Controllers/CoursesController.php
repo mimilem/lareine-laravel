e69bd486ad2e576bc26396fd0c94c3b64ec06b68
= <=>
2022-01-31 11:09:39 +0200

    [NEW] Add user verification page

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CoursesController extends Controller
 {
     public function index()
     {
         return view("courses.courses");
+    }
+
+    public function verify()
+    {
+        return view("courses.info-verification");
     }
 }
