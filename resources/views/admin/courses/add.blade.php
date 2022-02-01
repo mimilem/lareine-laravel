333f3c2eca8893e91973dc4ab1f956c0300559fb
= <=>
2022-02-01 12:05:22 +0200

    [NEW] Create add course page

---
+++ resources/views/admin/courses/add.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header">
+                <h3>Ajouter Une formation</h3>
+            </div>
+            <div class="card-body">
+                <div class="form-group row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <input type="text" placeholder="Titre de la formation" class="form-control">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
