4b0aca03a7f5cc162966b2b8f1587e6e5a50a3cb
= <=>
2022-02-05 13:24:19 +0200

    [ADD] Get last courses

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\Course;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $data = Course::all()->take(3);
+        
+        return view('home', $data = [
+            'courses' => $data
+        ]);
     }
 }
