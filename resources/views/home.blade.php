8af3bd5d512c93cb08276ad25c331a3fbc31d097
= <=>
2022-02-07 14:10:34 +0200

    [DELETE] Remove courses section on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -264,66 +264,6 @@
         </div>
     </div>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="pricing-tables medium-padding120 bg-border-color">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
-                            <div class="heading align-center">
-                                <h4 class="h2 heading-title">Formations disponibles</h4>
-                                <div class="heading-line">
-                                    <span class="short-line"></span>
-                                    <span class="long-line"></span>
-                                </div>
-                                <p class="heading-text">Découvrez nos formations.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        @foreach ($courses as $course)
-                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                                <div class="pricing-tables-item" style="background-color: white;">
-                                    <div class="pricing-tables-icon">
-                                        <img loading="lazy" src="{{ asset('assets/img/pricing1.png') }}" alt="personal">
-                                    </div>
-                                    <a href="#" class="pricing-title">{{ $course['title'] }}</a>
-                                    <ul class="pricing-tables-position">
-                                        <li class="position-item">
-                                            <span class="count">Lieu</span>
-                                            {{ $course['place'] }}
-                                        </li>
-                                        <li class="position-item">
-                                            <span class="count">Date</span>
-                                            {{ date('d-m-Y', strtotime($course['date'])) }}
-                                        </li>
-                                        <li class="position-item">
-                                            <span class="count">Heure</span>
-                                            {{ $course['start_time'] }}
-                                        </li>
-                                        <li class="position-item">
-                                            -
-                                        </li>
-                                        {{-- <li class="position-item">
-                                            <span class="count">15</span>
-                                            Social Accounts
-                                        </li> --}}
-                                    </ul>
-                                    <h4 class="rate">$ {{ $course['amount'] }}</h4>
-                                    <a href="{{ route('course_details', ['token' => $course['token']]) }}" class="btn btn-medium btn--dark">
-                                        <span class="text">Je souscris !</span>
-                                        <span class="semicircle"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="section">
         <div class="client-carousel medium-padding120">
             <div class="container">
@@ -335,7 +275,6 @@
                                 <span class="short-line"></span>
                                 <span class="long-line"></span>
                             </div>
-                            <p class="heading-text">Nos valeureux clients. </p>
                         </div>
                     </div>
                 </div>
