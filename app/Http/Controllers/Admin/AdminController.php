6488deec8773dbffbcb704c352b9d3f70c5599e6
= <=>
2022-02-14 15:47:45 +0200

    [ADD] Show stats on home page

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view("admin.index");
+        $courses = Course::all()->take(5);
+        $total_courses = Course::all()->count();
+
+        return view("admin.index", [
+            'courses' => $courses,
+            'total_courses' => $total_courses
+        ]);
     }
 }
