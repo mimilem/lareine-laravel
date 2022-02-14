8453d3a32ffb864d1c1e515650ade43b79b7d4bf
= <=>
2022-02-14 17:53:14 +0200

    [ADD] Add facilitators stat

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\Facilitator;
 
 class AdminController extends Controller
 {
@@ -12,10 +13,12 @@ class AdminController extends Controller
     {
         $courses = Course::all()->take(5);
         $total_courses = Course::all()->count();
+        $total_facilitators = Facilitator::all()->count();
 
         return view("admin.index", [
             'courses' => $courses,
-            'total_courses' => $total_courses
+            'total_courses' => $total_courses,
+            'total_facilitators' => $total_facilitators
         ]);
     }
 }
