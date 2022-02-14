0aa70e4c8c36fe0ffc3bd6325343c4732f4e6849
= <=>
2022-02-14 11:42:31 +0200

    [UPDATE] Change Marketing page content strucuture

--- resources/views/offers/marketing.blade.php
+++ resources/views/offers/marketing.blade.php
@@ -1,56 +1,106 @@
 @extends('layouts.master')
 @section('content')
-    <div class="container-fluid">
-        <div class="row medium-padding120 bg-border-color">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-                        <div class="heading mb30">
-                            <h4 class="h3 heading-title">Nos Missions en Stratégie Marketing</h4>
-                            <div class="heading-line">
-                                <span class="short-line"></span>
-                                <span class="long-line"></span>
-                            </div>
-                            <p class="heading-subtitle">En ligne avec notre dessein d’aider et d’accompagner
-                                les entreprises à créer et à maintenir la loyauté clientèle, nous intervenons
-                                en matière de stratégies marketing permettant aux entreprises de répondre aux
-                                besoins implicites ou explicites du marché qu’elles exploitent ou veulent exploiter.
-                            </p>
+<div style="position: relative">
+	<div class="stunning-header with-photo stunning-header-bg-photo1">
+		<div class="stunning-header-content">
+			<h1 class="stunning-header-title">Nos Missions en Stratégie Marketing</h1>
+			<p class="breadcrumbs-text">
+				Notre Mission en Stratégie Marketing est d’accompagner 
+                les entreprises à maintenir la fidélité de leur clientèle, d’accroitre ou d’affirmer 
+                leur notoriété en permettant à ces derniers de répondre aux attentes implicites ou 
+                explicites du marché qu’elles exploitent ou veulent exploiter.
+			</p>
+		</div>
+		<div class="overlay overlay-primary"></div>
+	</div>
+    <div class="container info-boxes pt100 pb100">
 
-                        </div>
-                        <ul class="list list--secondary mb60">
-                            <li>
-                                <i class="seoicon-check"></i>
-                                <a href="{{ route('customer_experience') }}">Expérience Client
-                                </a>
-                            </li>
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box1.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="{{ route('digital_marketing') }}" class="info-box-title">Marketing Digital</a>
+						<p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+							sed diam nonummy nibh euismod.
+						</p>
+					</div>
+				</div>
+			</div>
 
-                            <li>
-                                <i class="seoicon-check"></i>
-                                <a href="{{ route('branding') }}">Branding
-                                </a>
-                            </li>
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box2.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="{{ route('customer_experience') }}" class="info-box-title">Expérience Client</a>
+						<p class="text">Claritas est etiam processus dynamicus,
+							qui sequitur mutationem consuetudium lectorum.
+						</p>
+					</div>
+				</div>
+			</div>
 
-                            <li>
-                                <i class="seoicon-check"></i>
-                                <a href="{{ route('product_management') }}">Gestion des Produits
-                                </a>
-                            </li>
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box3.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="{{ route('branding') }}" class="info-box-title">Branding</a>
+						<p class="text">Investigationes demonstraverunt lectores
+							legere me lius quod ii legunt saepius notare.
+						</p>
+					</div>
+				</div>
+			</div>
 
-                            <li>
-                                <i class="seoicon-check"></i>
-                                <a href="{{ route('digital_marketing') }}">Marketing Digital
-                                </a>
-                            </li>
-                        </ul>
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box4.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="{{ route('product_management') }}" class="info-box-title">Gestion des Produits</a>
+						<p class="text">Claritas est etiam processus dynamicus,
+							qui sequitur mutationem consuetudium lectorum.
+						</p>
+					</div>
+				</div>
+			</div>
 
-                    </div>
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box5.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="#" class="info-box-title">Marketing Inbound</a>
+						<p class="text">Investigationes demonstraverunt lectores legere
+							me lius quod ii legunt saepius notare.
+						</p>
+					</div>
+				</div>
+			</div>
 
-                    <div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-12 col-xs-12">
-                        <img loading="lazy" src="{{ asset('assets/img/social-photo.png') }}" alt="social">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="info-box--standard" data-mh="info-boxes">
+					<div class="info-box-image">
+						<img loading="lazy" src="{{ asset('assets/img/info-box6.png') }}" alt="image">
+					</div>
+					<div class="info-box-content">
+						<a href="#" class="info-box-title">Vente Inbound</a>
+						<p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+							sed diam nonummy nibh euismod.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
 @endsection
