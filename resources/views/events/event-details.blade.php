226b5d71995ebbe0f9ebefb0ce825c7fa69783b9
= <=>
2022-02-16 17:14:37 +0200

    [UPDATE] Change Event details page design

--- resources/views/events/event-details.blade.php
+++ resources/views/events/event-details.blade.php
@@ -1,57 +1,150 @@
 @extends('layouts.master')
 @section('content')
-    <div class="container">
-        <div class="row pt80" >
-            <div class="product-description-ver3">
-                <div class="col-lg-12">
-                    <div class="heading align-center">
-                        <h4 class="h1 heading-title">CRDB Launch</h4>
-                        <div class="heading-line">
-                            <span class="short-line"></span>
-                            <span class="long-line"></span>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-lg-12">
-                    <div class="product-description-ver3-thumb">
-                        <img loading="lazy" src="{{ asset('assets/img/description-mac.png') }}" alt="mac-book">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="container-fluid">
-		<div class="row pt80 pb120 bg-border-color">
-
+		<div class="row bg-border-color medium-padding120">
 			<div class="container">
+				<div class="row">
+					<div class="product-description-ver2">
 
+						<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+							<div class="swiper-container" data-effect="fade">
 
-				<div class="row align-center">
+								<!-- Additional required wrapper -->
+								<div class="swiper-wrapper">
+									<div class="swiper-slide">
+										<div class="product-description-ver3-thumb">
+											<img loading="lazy" src="{{ asset('assets/img/description-mac.png') }}" alt="description" class="">
+										</div>
+									</div>
 
+								</div>
+								<!-- If we need pagination -->
+								<div class="swiper-pagination"></div>
+
+							</div>
+						</div>
+
+						<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-12 col-xs-12">
+							<div class="product-description-ver2-content">
+								<div class="heading">
+									<h4 class="h1 heading-title">CRDB Launch</h4>
+								</div>
+                                <div class="row">
+                                    <div class="product-details">
+                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div class="product-details-info">
+                                                <div class="product-details-add-info">
+                                                    <div class="author">Audience Ciblée :
+                                                        <span href="#" class="author-name">Lushois</span>
+                                                    </div>
+                                                    <div class="author">Facilitateur :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Date :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Heure de début :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Heure de fin :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Lieu :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Ville :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                    <div class="author">Province :
+                                                        <span href="#" class="author-name">...</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+    <div class="container">
+        
+    </div>
+
+    <div class="container-fluid">
+		<div class="row bg-border-color">
+			<div class="container">
+                <div class="row align-center">
 					<div class="col-lg-8 col-lg-offset-2">
 						<div class="heading">
-
 							<h5 class="mb30">Qolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibham liber tempor cum soluta
 								nobis eleifend option.
 							</h5>
-
 						</div>
-
 						<div class="likes-block">
 							<a href="#" class="btn btn-medium btn--dark btn-hover-shadow">
 								<span class="text">Souscrire</span>
 								<span class="semicircle"></span>
                             </a>
 						</div>
-
 					</div>
-
 				</div>
-
 			</div>
-
-
 		</div>
 	</div>
+    <div class="container">
+        <div class="row medium-padding120">
+    
+            <div class="col-lg-12">
+    
+                <div class="heading align-center">
+                    <h4 class="h4 heading-title">Nos Prochains Evenements</h4>
+                </div>
+                <div class="row">
+                    <a href="{{ route('event_details', ['id' => 1])}}" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <div class="case-item align-center mb60">
+                            <div class="case-item__thumb mouseover lightbox shadow animation-disabled">
+                                <img loading="lazy" src="{{ asset('assets/img/case1.jpg') }}" alt="our case">
+                            </div>
+                            <h6 class="case-item__title">Lancement CRDB</h6>
+                            <div class="case-item__cat">
+                                
+                            </div>
+                        </div>
+                    </a>
+    
+                    <a href="#" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <div class="case-item align-center mb60">
+                            <div class="case-item__thumb mouseover lightbox shadow animation-disabled">
+                                <img loading="lazy" src="{{ asset('assets/img/case3.jpg') }}" alt="our case">
+                            </div>
+                            <h6 class="case-item__title">Investigationes legere</h6>
+                            <div class="case-item__cat">
+                                
+                            </div>
+                        </div>
+                    </a>
+    
+                    <a href="#" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <div class="case-item align-center mb60">
+                            <div class="case-item__thumb mouseover lightbox shadow animation-disabled">
+                                <img loading="lazy" src="{{ asset('assets/img/case5.jpg') }}" alt="our case">
+                            </div>
+                            <h6 class="case-item__title">Investigationes legere</h6>
+                            <div class="case-item__cat">
+                                
+                            </div>
+                        </div>
+                    </a>
+    
+                </div>
+            </div>
+    
+        </div>
+    </div>
 @endsection
