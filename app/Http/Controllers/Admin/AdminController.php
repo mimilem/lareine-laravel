2af2b8c7fc175d063a327813e6b89b391f5689c9
= <=>
2022-02-17 15:55:37 +0200

    [ADD] Fix

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Activity;
-use App\Models\Facilitator;
 use App\Models\Subscribe;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -14,7 +14,7 @@ class AdminController extends Controller
     {
         $courses = Activity::all()->where('activity_type', 'COURSE')->take(5);
         $total_courses = Activity::all()->where('activity_type', 'COURSE')->count();
-        $total_facilitators = Facilitator::all()->count();
+        $total_facilitators = User::all()->where('user_role', 'ROLE_FACILITATOR')->count();
         $total_subscribes = Subscribe::all()->count();
 
         return view("admin.index", [
