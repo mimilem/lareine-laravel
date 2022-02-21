1771f0423de6fc4d78c47da097d2bee6fe4df950
= <=>
2022-02-21 15:12:43 +0200

    [ADD] Add table

--- resources/views/admin/forums/index.blade.php
+++ resources/views/admin/forums/index.blade.php
@@ -9,5 +9,51 @@
                 </div>
             </div>
         </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-block">
+                           <div class="row">
+                              <div class="col-sm-12 table-responsive">
+                                 <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Titre</th>
+                                            <th>Date</th>
+                                            <th>Lieu</th>
+                                            <th>Date de debut</th>
+                                            <th>Date de fin</th>
+                                            <th>Editer/Effacer</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {{-- @foreach ($courses as $course)
+                                            <tr>
+                                                <td>{{ $course['title'] }}</td>
+                                                <td>{{ date('d-m-Y', strtotime($course['date'])) }}</td>
+                                                <td>{{ $course['place'] }}</td>
+                                                <td>{{ $course['start_time'] }}</td>
+                                                <td>{{ $course['end_time'] }}</td>
+                                                <td>$ {{ $course['amount'] }}</td>
+                                                <td>
+                                                    <button type="button" class="btn btn-primary waves-effect waves-light">
+                                                        <i class="icofont icofont-edit"></i>
+                                                    </button>
+                                                    <button type="button" class="btn btn-danger waves-effect waves-light">
+                                                        <i class="icofont icofont-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endforeach --}}
+                                    </tbody>
+                                 </table>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
