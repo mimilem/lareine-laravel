7ae85055f5b49ff5275d891e65af986b96515161
elsa manang <manangelsa07@gmail.com>
2022-04-01 09:43:27 +0200

    ajout des textes marketing

--- resources/views/offers/marketing.blade.php
+++ resources/views/offers/marketing.blade.php
@@ -5,10 +5,9 @@
 		<div class="stunning-header-content">
 			<h1 class="stunning-header-title">Nos Missions en Stratégie Marketing</h1>
 			<p class="breadcrumbs-text">
-				Notre Mission en Stratégie Marketing est d’accompagner 
-                les entreprises à maintenir la fidélité de leur clientèle, d’accroitre ou d’affirmer 
-                leur notoriété en permettant à ces derniers de répondre aux attentes implicites ou 
-                explicites du marché qu’elles exploitent ou veulent exploiter.
+			Notre Mission en Stratégie Marketing est d’accompagner les entreprises à maintenir la fidélité de leur clientèle, 
+			d’accroitre ou d’affirmer leur notoriété en permettant à ces derniers de répondre aux attentes implicites ou explicites du marché qu’elles exploitent ou veulent exploiter. 
+			Les stratégies marketing.
 			</p>
 		</div>
 		<div class="overlay overlay-primary"></div>
@@ -23,8 +22,12 @@
 					</div>
 					<div class="info-box-content">
 						<a href="{{ route('digital_marketing') }}" class="info-box-title">Marketing Digital</a>
-						<p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-							sed diam nonummy nibh euismod.
+						<p class="text">: Exploitation, développement et positionnement d’un produit à travers le web. 
+							Nous définissons des stratégies de vente et de communication sur mesure à travers le digital afin d’accroitre la visibilité, le trafic, 
+							la notoriété des produits ainsi qu’assurer la croissance de l’audience cible. 
+							Nos principaux leviers sont : l’Emailing, la Publicité Display, Le Search Engine Marketing, 
+							le Social Media Marketing ainsi que le Web Analyse.
+.
 						</p>
 					</div>
 				</div>
@@ -37,8 +40,16 @@
 					</div>
 					<div class="info-box-content">
 						<a href="{{ route('customer_experience') }}" class="info-box-title">Expérience Client</a>
-						<p class="text">Claritas est etiam processus dynamicus,
-							qui sequitur mutationem consuetudium lectorum.
+						<p class="text"><b>Offrez une expérience client exceptionnelle pour fidéliser vos clients.</b>   
+							L’Expérience Client (XC) est l’ensemble des interactions entre un client et une marque, 
+							une entreprise et ses produits/services.
+							C’est avec l’expérience client que les meilleures entreprises se distinguent. En effet, 
+							celles-ci comprennent bien qu’une Expérience Client exceptionnelle n’est pas le fruit du hasard. 
+							Ces expériences exigent une stratégie holistique conçue pour amener vos clients à se sentir inspirés, 
+							bien traités et confiants. Beaucoup d’organisations internationale font appel à nous pour repenser leurs modèles d’affaires et les aider à mettre leurs clients au cœur de leurs activités. 
+							Nous préconisons une approche centrée sur l’humain qui intègre les personnes, 
+							la créativité et la technologie et qui vise à aider les entreprises à offrir une expérience client exceptionnelle de par sa conception. Si vous souhaitez offrir une meilleure expérience à vos clients, les fidéliser et augmenter vos revenus, il faut mettre en pratique des techniques pertinentes et imparables. Voici quelques bonnes pratiques qui vous aideront à y parvenir.
+
 						</p>
 					</div>
 				</div>
@@ -51,8 +62,16 @@
 					</div>
 					<div class="info-box-content">
 						<a href="{{ route('branding') }}" class="info-box-title">Branding</a>
-						<p class="text">Investigationes demonstraverunt lectores
-							legere me lius quod ii legunt saepius notare.
+						<p class="text">Une « brand » ou marque c’est d’abord une histoire. 
+							Confiez-nous d’abord son récit et nous vous aidons à bâtir votre plateforme de marque et à préciser les contours de sa personnalité, 
+							sa différence, la manière dont elle doit prendre la parole ; 
+							nous produisons un « pitch » qui rendra perceptible l’essence même de votre marque, 
+							qui guidera votre communication et donnera sens à l’engagement de vos publics. 
+							Notre solution de branding permet aux entreprises d’expliquer leur histoire en un instant, 
+							de créer un lien émotionnel, de renforcer la confiance, 
+							de toucher un public qui partage leurs valeurs’ ouvrir des nouvelles portes, 
+							de générer la croissance et enfin de créer une influence positive sur la morale de vos employés.
+
 						</p>
 					</div>
 				</div>
@@ -65,8 +84,12 @@
 					</div>
 					<div class="info-box-content">
 						<a href="{{ route('product_management') }}" class="info-box-title">Gestion des Produits</a>
-						<p class="text">Claritas est etiam processus dynamicus,
-							qui sequitur mutationem consuetudium lectorum.
+						<p class="text"><b>Identifiez les besoins du marché non satisfaits et rencontrez les opportunités des produits.</b>
+							Gardez une longueur d'avance sur la concurrence en intégrant les commentaires sur les produits dans chaque décision. 
+							Nous vus permettons d'analyser facilement les tendances du marché,
+							de suivre les concurrents, 
+							de hiérarchiser les fonctionnalités des produits et d'injecter des informations dans une stratégie de produit guidées par des données.
+
 						</p>
 					</div>
 				</div>
@@ -78,23 +101,12 @@
 						<img loading="lazy" src="{{ asset('assets/img/info-box5.png') }}" alt="image">
 					</div>
 					<div class="info-box-content">
-						<a href="#" class="info-box-title">Marketing Inbound</a>
-						<p class="text">Investigationes demonstraverunt lectores legere
-							me lius quod ii legunt saepius notare.
-						</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-				<div class="info-box--standard" data-mh="info-boxes">
-					<div class="info-box-image">
-						<img loading="lazy" src="{{ asset('assets/img/info-box6.png') }}" alt="image">
-					</div>
-					<div class="info-box-content">
-						<a href="#" class="info-box-title">Vente Inbound</a>
-						<p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-							sed diam nonummy nibh euismod.
+						<a href="#" class="info-box-title">Marketing Stratégique</a>
+						<p class="text">implanter ou renforcer les entreprises sur leur marché, 
+							définir leur positionnement dans l’esprit de leur cible et créer une valeur répondant à leurs besoins, 
+							désirs et attentes. S’assurer des méthodes et techniques adéquates pour permettre aux entreprises de gagner. 
+							Il n'est pas nécessaire de gérer plusieurs agences ou de sous-traiter à divers fournisseurs de services. 
+							Nos capacités et nos compétences constituent la base d'une approche globale conçue pour vous offrir un service complet et une expérience en agence.
 						</p>
 					</div>
 				</div>
